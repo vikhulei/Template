3a6907b955b077985376a40067f7f8f1bb97bd2b
vikhulei <vikhulei@gmail.com>
2026-09-10 20:45:05 +0100

    footer top part completed

--- frontend/src/components/layout/footer/Footer.jsx
+++ frontend/src/components/layout/footer/Footer.jsx
@@ -1,33 +1,62 @@
-import { FooterOuter, FooterInner, FooterColumn, FooterHeader, FooterLink, SocialRow, ContactItem } from "./Footer.styles"
+import { FooterOuter, FooterInner, FooterTop, FooterColumn, FooterHeader, FooterLink, SocialRow, FooterBottom } from "./Footer.styles"
+import {
+    FaFacebookF,
+    FaTwitter,
+    FaInstagram,
+    FaYoutube,
+    FaLinkedinIn
+} from "react-icons/fa";
+
 
 
 function Footer() {
 
     const FooterContent = [
-        {heading: "LinquaPro", text1: "About Us", href2: "#", text2: "Courses", href3: "#", text3: "How It Works", href4: "#",},
-        {heading: "Quick Links", text1: "About Us2", href2: "#", text2: "Courses", href3: "#", text3: "How It Works", href4: "#", text4: "Pricing", href5: "#", text5: "Contact", href1: "#",},
-        {heading: "Resources", text1: "About Us3", href2: "#", text2: "Courses", href3: "#", text3: "How It Works", href4: "#", text4: "Pricing", href5: "#", text5: "Contact", href1: "#",},
-        {heading: "Support", text1: "About Us4", href2: "#", text2: "Courses", href3: "#", text3: "How It Works", href4: "#", text4: "Pricing", href5: "#", text5: "Contact", href1: "#",},
-        {heading: "Contact Us", text1: "About Us5", href2: "#", text2: "Courses", href3: "#", text3: "How It Works", href4: "#", text4: "Pricing", href5: "#", text5: "Contact", href1: "#",},
+        { heading: "LinquaPro", text1: "We help people around the world learn English and build a better future through education", },
+        { heading: "Resources", text1: "About Us2", href1: "#", text2: "Google", href2: "https://www.google.com", target2: "_blank", href3: "#", text4: "Pricing", href4: "#", text5: "Contact", href5: "#", },
+        { heading: "Support", text1: "About Us2", href1: "#", text2: "Courses", href2: "#", text3: "How It Works", href3: "#", text4: "Pricing", href4: "#", text5: "Contact", href5: "#", },
+        { heading: "Contact us", text1: "About Us2", href1: "#", text2: "Courses", href2: "#", text3: "How It Works", href3: "#", text4: "Pricing", href4: "#", text5: "00000000", },
+    ]
+
+    const SocialIcons = [
+        { icon: <FaFacebookF />, href: "https://www.google.com" },
+        { icon: <FaTwitter />, href: "https://www.google.com" },
+        { icon: <FaInstagram />, href: "https://www.google.com" },
+        { icon: <FaYoutube />, href: "https://www.google.com" },
+        { icon: <FaLinkedinIn />, href: "https://www.google.com" },
     ]
 
 
-  return (
-    <FooterOuter>
-        <FooterInner>
-            {FooterContent.map((value, index) => (
-                <FooterColumn key={index}>
-                    <FooterHeader>{value.heading}</FooterHeader>
-                    <FooterLink href={value.href1}>{value.text1}</FooterLink>
-                    <FooterLink href={value.href2}>{value.text2}</FooterLink>
-                    <FooterLink href={value.href3}>{value.text3}</FooterLink>
-                    <FooterLink href={value.href4}>{value.text4}</FooterLink>
-                    <FooterLink href={value.href5}>{value.text5}</FooterLink>
-                </FooterColumn>
-            ))}
-        </FooterInner>
-    </FooterOuter>
-  )
+    return (
+        <FooterOuter>
+            <FooterInner>
+                <FooterTop>
+                    {FooterContent.map((value, index) => (
+                        <FooterColumn key={index}>
+                            <FooterHeader>{value.heading}</FooterHeader>
+                            <FooterLink href={value.href1}>{value.text1}</FooterLink>
+                            <FooterLink href={value.href2} target="_blank">{value.text2}</FooterLink>
+                            <FooterLink href={value.href3}>{value.text3}</FooterLink>
+                            <FooterLink href={value.href4}>{value.text4}</FooterLink>
+                            <FooterLink href={value.href5}>{value.text5}</FooterLink>
+                            {index === 0 && (
+                                <SocialRow>
+                                    {SocialIcons.map((value, index) => (
+                                        <a key={index} href={value.href} target="_blank">
+                                            {value.icon}
+                                        </a>
+                                    ))}
+                                </SocialRow>
+                            )}
+                        </FooterColumn>
+                    ))}
+                </FooterTop>
+                <FooterBottom>
+                    Bottom lines will be placed here
+                </FooterBottom>
+            </FooterInner>
+        </FooterOuter>
+    )
 }
 
 export default Footer
--- frontend/src/components/layout/footer/Footer.styles.js
+++ frontend/src/components/layout/footer/Footer.styles.js
@@ -7,33 +7,46 @@ const FooterOuter = styled(Section)`
 `
 
 const FooterInner = styled(Container)`
+    padding: 20px 10vw;
+    flex-direction: column;
+`
+
+const FooterTop = styled.div`
     display: grid;
-    grid-template-columns: repeat(5, auto);
+    margin: auto;
+    width: 100%;
+    grid-template-columns: repeat(4, auto);
     justify-content: space-between;
     align-items: start;
-    padding: 40px;
 `
 
 const FooterColumn = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+    max-width: 200px;
 `
 
 const FooterHeader = styled.div`
     color: white;
+    margin-bottom: 10px;
 `
 
 const FooterLink = styled.a`
+    font-size: 0.7rem;
     color: white;
+    line-height: 1.5;
 `
 
 const SocialRow = styled.div`
-
+    color: white;
+    display: flex;
+    gap: 20px;
+    padding-top: 10px;
 `
 
-const ContactItem = styled.div`
-
+const FooterBottom = styled.div`
+    color: white;
 `
 
-export { FooterOuter, FooterInner, FooterColumn, FooterHeader, FooterLink, SocialRow, ContactItem }
+export { FooterOuter, FooterInner, FooterTop, FooterColumn, FooterHeader, FooterLink, SocialRow, FooterBottom }
