cab6f773837448e7970dbb89e30aea17d57d573e
vikhulei <vikhulei@gmail.com>
2026-09-18 19:43:21 +0100

    hero styles drafted

--- frontend/src/pages/home/sections/01_hero/Hero.styles.js
+++ frontend/src/pages/home/sections/01_hero/Hero.styles.js
@@ -1,3 +1,48 @@
 import styled from "styled-components";
+import { Container, Section } from "../../../../design/primitives/Primitives";
+
+const HeroOuter = styled(Section)`
+
+`
+
+const HeroInner = styled(Container)`
+
+`
+
+const HeroContent = styled.div`
+
+`
+
+const Eyebrow = styled.div`
+
+`
+
+const HeroTitle = styled.div`
+
+`
+
+const HeroDescription = styled.div`
+
+`
+
+const HeroActions = styled.div`
+
+`
+
+const SocialProof = styled.div`
+
+`
+
+const HeroVisual = styled.div`
+
+`
+
+const HeroImage = styled.div`
+
+`
+
+const CourseProgress = styled.div`
+
+`
 
 export {  }
