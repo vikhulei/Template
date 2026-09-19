11585a5245adef6a669960dac4b878efe78fbec7
vikhulei <vikhulei@gmail.com>
2026-09-19 13:51:16 +0100

    TrystedBy styles drafted

--- frontend/src/pages/home/sections/02_trustedBy/TrustedBy.styles.js
+++ frontend/src/pages/home/sections/02_trustedBy/TrustedBy.styles.js
@@ -1,3 +1,24 @@
 import styled from "styled-components"
+import { Container, Section } from "../../../../design/primitives/Primitives"
 
-export {  }
+const TrustedByOuter = styled(Section)`
+
+`
+
+const TrustedByInner = styled(Container)`
+
+`
+
+const TrustedByTitle = styled.div`
+
+`
+
+const LogoList = styled.div`
+
+`
+
+const LogoImage = styled.img`
+
+`
+
+export { TrustedByOuter, TrustedByInner, TrustedByTitle, LogoList, LogoImage }
