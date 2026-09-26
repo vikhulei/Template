b8a50f3036deb1e3a8571d220a1bf3cecbbc4267
vikhulei <vikhulei@gmail.com>
2026-09-26 12:52:21 +0100

    sections imported to Home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
 import Hero from "./sections/01_hero/Hero"
+import TrustedBy from "./sections/02_trustedBy/TrustedBy"
+import Benefits from "./sections/03_benefits/Benefits"
+import Courses from "./sections/04_courses/Courses"
+import Cta from "./sections/05_cta/Cta"
+import SuccessStories from "./sections/06_successStories/SuccessStories"
+import Newsletter from "./sections/07_newsletter/Newsletter"
 
 function Home() {
   return (
     <>
       <Hero />
+      <TrustedBy />
+      <Benefits />
+      <Courses />
+      <Cta />
+      <SuccessStories />
+      <Newsletter />
     </>
   )
 }
